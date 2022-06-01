7387e3527f254ef293fdc8a26201ce0f06374416
tttcowan <tom@appsauce.co.uk>
2022-06-01 08:55:59 +0100

    Remove unused imports

--- library/src/main/java/eightbitlab/com/blurview/RenderEffectBlur.java
+++ library/src/main/java/eightbitlab/com/blurview/RenderEffectBlur.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
